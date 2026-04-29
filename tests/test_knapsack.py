e09ffce818229cfc1233d144ab60449ebeb57d9c
JOSE RODOLFO DE LA CRUZ <de428179@uaeh.edu.mx>
2026-04-29 15:10:49 -0600

    Add basic tests for mochila_greedy function

---
+++ tests/test_knapsack.py
@@ -0,0 +1,11 @@
+from heuristicos_pkg.knapsack import mochila_greedy
+
+def test_mochila_basico():
+    valores = [60, 100, 120]
+    pesos = [10, 20, 30]
+    capacidad = 50
+
+    seleccion, valor = mochila_greedy(valores, pesos, capacidad)
+
+    assert valor > 0
+    assert sum(pesos[i] for i in seleccion) <= capacidad
