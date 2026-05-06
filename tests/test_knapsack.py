cdcebe7ae8244f70479c5c74ed76f879fbde7ab6
JOSE RODOLFO DE LA CRUZ <de428179@uaeh.edu.mx>
2026-05-06 17:04:06 -0600

    Add test for simulated annealing knapsack solution

--- tests/test_knapsack.py
+++ tests/test_knapsack.py
@@ -20,3 +20,25 @@ def test_mochila_backtracking():
 
     assert peso <= capacidad
     assert valor == 220
+
+from heuristicos_pkg.knapsack import mochila_recocido_simulado
+
+def test_mochila_recocido():
+    valores = [60, 100, 120]
+    pesos = [10, 20, 30]
+    capacidad = 50
+
+    solucion, valor = mochila_recocido_simulado(
+        valores,
+        pesos,
+        capacidad
+    )
+
+    peso_total = sum(
+        pesos[i]
+        for i in range(len(solucion))
+        if solucion[i] == 1
+    )
+
+    assert peso_total <= capacidad
+    assert valor > 0
