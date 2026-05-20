6f290308d08c52b33795385d82174ef140ee112e
MARCELA MENDOZA ROQUE <233203375+me400582-arch@users.noreply.github.com>
2026-05-20 08:59:43 -0600

    Refactor TSP class and methods for better structure

--- heuristicos_pkg/viajero.py
+++ heuristicos_pkg/viajero.py
@@ -1,56 +1,57 @@
 import random
 
+class TSP:
+    def __init__(self, matriz_costos):
+        """Inicializa el problema con la matriz de distancias"""
+        self.M = matriz_costos
 
-def generar_ruta(n):
-    """Genera una permutación válida de ciudades"""
-    ruta = list(range(n))
-    random.shuffle(ruta)
-    return ruta
+    def generar_ruta(self, n):
+        """Genera una permutación válida de ciudades"""
+        ruta = list(range(n))
+        random.shuffle(ruta)
+        return ruta
 
+    def generar_poblacion(self, tamano_poblacion, n):
+        """Genera una población de rutas válidas"""
+        poblacion = []
+        for _ in range(tamano_poblacion):
+            ruta = self.generar_ruta(n)
+            poblacion.append(ruta)
+        return poblacion
 
-def generar_poblacion(tamano_poblacion, n, M=None):
-    """Genera una población de rutas válidas"""
-    poblacion = []
-    for _ in range(tamano_poblacion):
-        ruta = generar_ruta(n)
-        poblacion.append(ruta)
-    return poblacion
+    def calcular_costo(self, ruta):
+        """Calcula el costo total de una ruta (ciclo cerrado)"""
+        costo = 0
+        n = len(ruta)
 
+        for i in range(n):
+            origen = ruta[i]
+            destino = ruta[(i + 1) % n]  # regresa al inicio
+            costo += self.M[origen][destino]
 
-def calcular_costo(ruta, M):
-    """Calcula el costo total de una ruta (ciclo cerrado)"""
-    costo = 0
-    n = len(ruta)
+        return costo
 
-    for i in range(n):
-        origen = ruta[i]
-        destino = ruta[(i + 1) % n]  # regresa al inicio
-        costo += M[origen][destino]
+    def dos_opt(self, ruta):
+        """Mejora una ruta usando 2-OPT"""
+        mejor = ruta[:]
+        mejor_costo = self.calcular_costo(mejor)
 
-    return costo
+        mejora = True
+        while mejora:
+            mejora = False
 
+            for i in range(1, len(ruta) - 1):
+                for j in range(i + 1, len(ruta)):
+                    nueva_ruta = mejor[:]
+                    nueva_ruta[i:j] = reversed(nueva_ruta[i:j])
 
-def dos_opt(ruta, M):
-    """Mejora una ruta usando 2-OPT"""
-    mejor = ruta[:]
-    mejor_costo = calcular_costo(mejor, M)
+                    nuevo_costo = self.calcular_costo(nueva_ruta)
 
-    mejora = True
-    while mejora:
-        mejora = False
+                    if nuevo_costo < mejor_costo:
+                        mejor = nueva_ruta
+                        mejor_costo = nuevo_costo
+                        mejora = True
 
-        for i in range(1, len(ruta) - 1):
-            for j in range(i + 1, len(ruta)):
-                nueva_ruta = mejor[:]
-                nueva_ruta[i:j] = reversed(nueva_ruta[i:j])
+            ruta = mejor
 
-                nuevo_costo = calcular_costo(nueva_ruta, M)
-
-                if nuevo_costo < mejor_costo:
-                    mejor = nueva_ruta
-                    mejor_costo = nuevo_costo
-                    mejora = True
-
-        ruta = mejor
-
-    return mejor, mejor_costo
+        return mejor, mejor_costo
